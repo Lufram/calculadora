54610f83b6364cb6131f4f2df728523925f4162d
jabaron56 <jabaron56@gmail.com>
2021-02-26 10:59:09 +0100

    Added TestResta

---
+++ src/TestCalc/TestCalc.java
@@ -0,0 +1,14 @@
+package TestCalc;
+
+import Calculadora.Resta;
+
+public class TestCalc {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Primer método : " + Resta.restar(5,4) );
+		System.out.println("Segundo método : " + Resta.restar(7, 3) );
+		
+	}
+
+}
